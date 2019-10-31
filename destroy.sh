2c946fa2a81c4b7f0af9f727065590801771389e
Prashanth-Mallyampatti <prashanth.m1996@gmail.com>
2019-10-31 18:11:58 +0000

    Working code

---
+++ destroy.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+virsh net-undefine internet
+virsh net-undefine other
+virsh net-undefine l2
+virsh net-undefine l3
+
+virsh net-destroy internet
+virsh net-destroy other
+virsh net-destroy l2
+virsh net-destroy l3
+
+ovs-vsctl del-br SW1
+ovs-vsctl del-br SW2
+ovs-vsctl del-br SW3
+ovs-vsctl del-br SW4
+
+virsh undefine AnsibleVM1
+virsh undefine AnsibleVM2
+
+virsh destroy AnsibleVM1
+virsh destroy AnsibleVM2
